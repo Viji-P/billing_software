5ae4132b8c6ed8c38dc895a51aa19226b7546457
Viji-P <mpvijayalakshmi91103@gmail.com>
2025-02-19 14:29:19 +0530

    some modification

--- billing-system/src/main/java/com/example/billing_system/services/UserService.java
+++ billing-system/src/main/java/com/example/billing_system/services/UserService.java
@@ -18,16 +18,12 @@ public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    @Autowired  // Constructor Injection - BEST PRACTICE
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-    public String encodePassword(String rawPassword) {
-        return passwordEncoder.encode(rawPassword);
-    }
-
+    
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
